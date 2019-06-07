4e46874d9d89d6ffe84f290336873722c1247a33
Thomas CHAIGNEAU <t.chaigneau@groupeonepoint.com>
2019-06-07 20:03:23 +0200

    [refacto] event repetition

--- api/controllers/eventsController.js
+++ api/controllers/eventsController.js
@@ -1,13 +1,42 @@
-const Models = require('../models/')
+const moment = require('moment')
+const { Events } = require('../models/')
 
-const { Events } = Models 
+exports.postEvent = (req, res, next) => {
+    const event = req.body
+    console.log('------POST------')
+    console.log(event)
+    console.log('------POST END------')
+
+    try {
+        if (event.frequency === 'daily') {
+            const endingDate = moment(event.startingDate).add(event.amount, event.unitOfTime);
+            const repetitionTimes = endingDate.diff(event.startingDate, 'days');
+            const promises = Array(repetitionTimes).fill(0).map((_, index) => Events.create({
+                ...event,
+                startingDate: moment(event.startingDate, 'YYYY-MM-DD').add(index, 'days').format('YYYY-MM-DD')
+            }));
+
+            return Promise.all(promises)
+                .then(events => res.send(events))
+                .catch(err => next(err))
+
+        } else {
+            Events
+            .create(event)
+            .then(() => res.status(200).send([event]))
+        }
+
+    } catch (err) {
+        console.log('ERROR IN POST EVENT:', err)
+        res.status(400).send(err)
+    }
+}
 
 exports.getAllEvents  = (req, res) => {
 	try {
         Events
         .findAll({})
         .then((events) => {
-            console.log('events---', events)
             res.status(200).json(events)
         })
 	} catch (err) {
@@ -16,14 +45,3 @@ exports.getAllEvents  = (req, res) => {
 	}
 }
 
-exports.postEvent = (req, res) => {
-    const event = req.body
-    try {
-        Events
-        .create(event)
-        .then(() => res.status(200).send(event))
-    } catch (err) {
-        console.log('ERROR IN POST EVENT:', err)
-        res.status(400).send(err)
-    }
-}
--- api/models/eventsModel.js
+++ api/models/eventsModel.js
@@ -3,9 +3,9 @@ module.exports = (sequelize, DataTypes) => {
   const Events = sequelize.define('Events', {
     title: DataTypes.STRING,
     startingDate: DataTypes.STRING,
-    endingDate: DataTypes.STRING,
+    startingHour: DataTypes.STRING,
+    duration: DataTypes.STRING,
     frequency: DataTypes.STRING,
-    daysSelected: DataTypes.STRING,
   }, {});
   Events.associate = function(models) {
     // associations can be defined here
